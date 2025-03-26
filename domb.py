2ccd600591b6c98d71cc0b6c51e995b6e98024f1
Gellen69 <gama-kovacs.gellen@diak.szbi-pg.hu>
2025-03-26 14:51:46 +0100

    nagy csecsek👌

---
+++ domb.py
@@ -0,0 +1,27 @@
+# obet = "O "
+# for i in range(1, 5):
+#     print(obet * i)
+# for i in range(4, 1, - 1):
+#     print(obet * i)
+
+# domború csecs
+
+hanyszor = int(input("mekkora csöcs kell?"))
+print(type(hanyszor))
+
+obet = "O "
+for i in range(1, hanyszor):
+    print(obet * i)
+for i in range(hanyszor - 1, 0, - 1):
+     print(obet * i)
+
+# rakéta csecs
+
+# hanyszor = 6
+# print(type(hanyszor))
+
+# obet = "O "
+# for i in range(1, hanyszor):
+#     print(obet * i)
+# for i in range(hanyszor, 0, - 1):
+#      print(obet * i)
---
+++ for_ciklus.py
@@ -0,0 +1,26 @@
+#irass ki egymás után 10 számot!
+
+# for i in range(10):
+#   print(i)
+
+#irass ki egymás után 1-10-ig számokat!
+
+# for i in range(1, 11):
+#   print(i)
+
+#irass ki egymás után 1-10-ig páros számokat!
+
+# for i in range(2, 11, 2):
+#   print(i)
+
+#irass ki egymás után 10-1-ig páros számokat!
+
+# for i in range(10, 0, - 1):
+#   print(i)
+
+# O 
+# O O 
+# O O O 
+# O O O O 
+# O O O O 
+
